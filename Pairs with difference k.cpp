161a233ccbc408db64f3ec7afe3d17bbb7189ffb
Aman Raj <iamanraj01@gmail.com>
2024-10-30 14:04:49 +0530

    Update Pairs with difference k.cpp

--- Pairs with difference k.cpp
+++ Pairs with difference k.cpp
@@ -2,13 +2,15 @@ class Solution {
   public:
     /* Returns count of pairs with difference k  */
     int countPairsWithDiffK(vector<int>& arr, int k) {
-        int count=0,n=arr.size();
-        unordered_map<int,int>mp;
-        for(int i=0;i<n;i++){
-            count+=mp[arr[i]-k];
-            count+=mp[arr[i]+k];
-            mp[arr[i]]++;
-        }
-        return count;
+       unordered_map<int,int>mp;
+       int ans=0;
+       for(int i=0;i<arr.size();i++){
+           mp[arr[i]]++;
+       }
+       for(int i=0;i<arr.size();i++){
+           if(mp.find(arr[i]+k)!=mp.end())
+           ans+=mp[arr[i]+k];
+       }
+       return ans;
     }
 };
