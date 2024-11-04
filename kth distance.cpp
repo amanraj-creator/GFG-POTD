cb8b17442738e15e085ce3c75786fee2e69e50ff
Aman Raj <iamanraj01@gmail.com>
2024-11-04 13:03:06 +0530

    Update kth distance.cpp

--- kth distance.cpp
+++ kth distance.cpp
@@ -1,18 +1,16 @@
+
 class Solution {
   public:
 
     bool checkDuplicatesWithinK(vector<int>& arr, int k) {
-      unordered_map<int,int> map;
-        map[0] = -1; // best practice
-        for(int i=0;i<arr.size();i++){
-            //  main condition
-            if(map.find(arr[i]) != map.end()){
-                // duplicate value within the k distance
-                if(i - map[arr[i]] <= k) return 1;
-            }
-            // updating the old index value with new Value
-            map[arr[i]] = i; // insert value
-        }
-        return 0;  
+       unordered_map<int,int>mp;
+       for(int i=0;i<arr.size();i++){
+           if(mp.find(arr[i])!=mp.end()){
+               if(i-mp[arr[i]]<=k)
+               return true;
+           }
+           mp[arr[i]]=i;
+       }
+       return false;
     }
 };
