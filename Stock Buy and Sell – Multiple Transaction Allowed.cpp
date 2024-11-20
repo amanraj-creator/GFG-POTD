d8ba7415df63b93bc44007bc56a3bd397bd176f5
Aman Raj <iamanraj01@gmail.com>
2024-11-21 00:57:00 +0530

    Create Stock Buy and Sell – Multiple Transaction Allowed.cpp

---
+++ Stock Buy and Sell – Multiple Transaction Allowed.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    int maximumProfit(vector<int> &prices) {
+        int maxi=0;
+        for(int i=1;i<prices.size();i++){
+            if(prices[i]>prices[i-1]){
+                maxi+= prices[i]-prices[i-1];
+            }
+        }
+        return maxi;
+    }
+};
