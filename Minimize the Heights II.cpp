181f5741d738ba965883234ae3c13942dfe6c7af
Aman Raj <iamanraj01@gmail.com>
2024-11-23 13:48:51 +0530

    Update Minimize the Heights II.cpp
    updates

--- Minimize the Heights II.cpp
+++ Minimize the Heights II.cpp
@@ -1,21 +1,24 @@
 class Solution {
   public:
     int getMinDiff(vector<int> &arr, int k) {
-        int n=arr.size();
-        if(n==1) return 0; //base case
-        
-        sort(arr.begin(),arr.end());
-        int res=arr[n-1]-arr[0];
-        int low=arr[0]+k;
-        int high=arr[n-1]-k;
-        
-        for(int i=1;i<n;i++){
-          int mini=min(low,arr[i]-k);
-          int maxi=max(arr[i-1]+k,high);
-          if(mini<0) continue;
-          
-          res=min(res,maxi-mini);
-        }
-        return res;
+      int n=arr.size();
+      if(n==1)return 0; // if there's only one tower
+      //sortung the array
+      sort(arr.begin(),arr.end());
+// Initialize the result as the difference between the max and min height in the sorted array.
+      int res=arr[n-1]-arr[0];
+ // The lowest possible height and highest possible height after adjusting with k  
+      int low=arr[0]+k;
+      int high=arr[n-1]-k;
+      // traversing array
+      for(int i=0;i<n-1;i++){
+          int mini=min(low,arr[i+1]-k);
+          int maxi=max(high,arr[i]+k);
+    // If the new height is negative, skip to avoid invalid cases
+     if(mini < 0) continue;
+      // Update the result with the minimum possible difference
+            res = min(res, maxi - mini);
+      }
+      return res;
     }
 };
