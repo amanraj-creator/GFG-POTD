c0a8aa56af0c818c5a5c17eebcf532438ad23fd5
Aman Raj <iamanraj01@gmail.com>
2024-12-20 22:24:43 +0530

    Update Spirally traversing a matrix.cpp

--- Spirally traversing a matrix.cpp
+++ Spirally traversing a matrix.cpp
@@ -1,54 +1,34 @@
 class Solution {
   public:
-    vector<int> spirallyTraverse(vector<vector<int>> &matrix)
-    {
-        // code here
-     
-        int left=0;
-        int right=matrix[0].size()-1;
-        int top=0;
-        int bottom=matrix.size()-1;
-        
+    vector<int> spirallyTraverse(vector<vector<int> > &mat) {
+        int left=0, right=mat[0].size()-1; // last column
+        int top=0, bottom=mat.size()-1; // last row
         vector<int>ans;
-        
-        while(left<=right && top<=bottom)
-        {
-           for(int i=left; i<=right; i++)
-           {
-               
-               ans.push_back(matrix[top][i]);
+        while(left<=right && top<=bottom){
+            //step1
+           for(int i=left; i<=right; i++){
+              ans.push_back(mat[top][i]);
            }
            top++;
-           
-           for(int i=top;i<=bottom;i++)
-           {
-               ans.push_back(matrix[i][right]);
-               
+            //step2
+           for(int i=top;i<=bottom;i++){
+               ans.push_back(mat[i][right]);
            }
            right--;
-           
-           if(top<=bottom)
-            {
-               for(int i=right;i>=left;i--) 
-               {
-                   ans.push_back(matrix[bottom][i]);
-               }
+           if(top<=bottom){ //step3
+               for(int i=right;i>=left;i--){
+                   ans.push_back(mat[bottom][i]);
+                }
                bottom--; 
             }
-            
-            if(left<=right)
-            {
-              for(int i=bottom;i>=top;i--)
-              {
-                  
-                  
-                  ans.push_back(matrix[i][left]);
-              }
+            if(left<=right){ //step4
+              for(int i=bottom;i>=top;i--){
+                  ans.push_back(mat[i][left]);
+               }
                left++; 
-                
             }
-        }
-            
+         }
           return ans;
-    }
-};
+      }
+   };
+   
