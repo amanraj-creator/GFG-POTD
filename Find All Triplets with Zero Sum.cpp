2e06b194526989a968e2507269689dc53eb9cfbb
Aman Raj <iamanraj01@gmail.com>
2024-12-28 13:43:00 +0530

    Update Find All Triplets with Zero Sum.cpp

--- Find All Triplets with Zero Sum.cpp
+++ Find All Triplets with Zero Sum.cpp
@@ -1,19 +1,16 @@
 class Solution {
   public:
     vector<vector<int>> findTriplets(vector<int> &arr) {
-     int n = arr.size();
-        vector<vector<int>> ans;
-        
-        for(int i = 0; i < n; i++) {
-            for(int j = i + 1; j < n; j++) {
-                for(int k = j + 1; k < n; k++) {
-                    int sum = arr[i] + arr[j] + arr[k];
-                    if(sum == 0) {
-                        ans.push_back({i, j, k});
-                    }
-                }
-            }
-        }
-        return ans;   
+        vector<vector<int>>ans;
+         int n=arr.size();
+         for(int i=0;i<n-2;i++){
+             for(int j=i+1;j<n-1;j++){
+                 for(int k=j+1;k<n;k++){
+                     if(arr[i]+arr[j]+arr[k]==0)
+                     ans.push_back({i,j,k});
+                 }
+             }
+         } 
+         return ans;
     }
 };
