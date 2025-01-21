e3065188020893fcd8dae9f77a856f4156e1e602
Aman Raj <iamanraj01@gmail.com>
2025-01-21 11:31:03 +0530

    Update Rotate a Linked List.cpp

--- Rotate a Linked List.cpp
+++ Rotate a Linked List.cpp
@@ -1,21 +1,20 @@
 class Solution {
   public:
-    
     Node* rotate(Node* head, int k) {
-      Node* temp = head;
-        
-        while(temp->next != NULL)
-        {
-            temp = temp->next;
+        if(k==0 ||!head ||!head->next) return head;
+        Node *temp=head;
+        int cnt=1;
+        while(temp->next){
+            temp=temp->next; // TEMP ko last me lana h
+            cnt++; // count of nodes
         }
-        while(temp != NULL && k != 0)
-        {
-            temp->next = head;
-            head = head->next;
-            temp = temp->next;
-            k--;
-        }
-        temp->next = NULL;
-        return head; 
+        k=k%cnt; // no of actual rotation
+        if(k==0) return head;
+        temp->next=head;  //  make circular ll
+        Node* newTail=head;
+        for(int i=1;i<k;i++) newTail=newTail->next;
+        Node* newHead=newTail->next;
+        newTail->next=nullptr; // TO REMOVE CIRCULAR LL
+        return newHead; 
     }
 };
